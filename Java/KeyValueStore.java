eb9ec80f040d8b90a2f16f5aac4cfe108cabab50
aarif123456 <abdullahmeo11@gmail.com>
2020-01-31 17:13:52 -0500

    added reading from config file io and refactored the code a bit

--- Java/KeyValueStore.java
+++ Java/KeyValueStore.java
@@ -1,38 +1,43 @@
-// Abdullah Arif
-// Custom key value store structure for COMP-4680
-// T is the type for the key and G is the type for the Value
+/* Abdullah Arif
+* Custom key value store structure for COMP-4680
+* T is the type for the key and G is the type for the Value */
 
-import java.util.*;
-import com.google.gson.*;
-public class KeyValueStore<T,G>{
-    HashMap<T,G> map;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.HashMap;
+
+public class KeyValueStore<T, G> {
+
+    final HashMap<T, G> map;
+
     //Create
-    public KeyValueStore(){ 
-        map = new HashMap<T,G>();
+    public KeyValueStore() {
+        map = new HashMap<>();
     }
 
     // Insert ​- enables the user to insert a key and corresponding value into the store
-    public synchronized void insert(T key, G value){
+    public synchronized void insert(T key, G value) {
         map.put(key, value);
     }
 
     //Get ​- enables the sure to retrieve a value using the corresponding key
-    public G get(T key){
+    public G get(T key) {
         return map.get(key);
     }
 
     // Delete ​- enables the user to delete a value from the store using the corresponding key.
-    public synchronized G delete(T key){
+    public synchronized G delete(T key) {
         return map.remove(key);
     }
 
     // Find​ - enables the user to check if a key exists in the store or not
-    public boolean find(T key){
+    public boolean find(T key) {
         return map.containsKey(key);
     }
+
     // Update ​- enables the user to update the value of an existing key.
-    public synchronized boolean update(T key, G newValue){
-        if(map.containsKey(key)){
+    public synchronized boolean update(T key, G newValue) {
+        if (map.containsKey(key)) {
             map.replace(key, newValue);
             return true;
         }
@@ -41,31 +46,31 @@ public class KeyValueStore<T,G>{
 
     // UpSert - enables the user to update the value of an existing key and if the key does not exist,
     // it will insert the new value with the corresponding key
-    public synchronized boolean upSert(T key, G newValue){
-        if(map.containsKey(key)){
+    public synchronized void upSert(T key, G newValue) {
+        if (map.containsKey(key)) {
             map.replace(key, newValue);
-            return false; // key was not create
+            return; // key was not create
         }
-       // if there was no previous value
-        map.put(key,newValue);
-        return true;
+        // if there was no previous value
+        map.put(key, newValue);
     }
 
     //Clear ​- enables​​ the user to remove all the  key-value items stored in the key-value store
-    public synchronized void clear(){
+    public synchronized void clear() {
         map.clear();
     }
 
     //Count​ returns the number of key-value items stored in the key-value store
-    public int count(){
+    public int count() {
         return map.size();
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return  gson.toJson(map); 
+        return gson.toJson(map);
     }
-    
+
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Distributed-DB
 Distributed database for networks course
+Broken down into 4 parts
+1. Creating key-store value with given operation: 
+2. Create multi-threaded client server application
+3. 
+4. 
+
+Implemented in 3 languages: C++, Java, Python
