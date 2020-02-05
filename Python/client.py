a631511f3067af2a0c70bde578d3b668b6b71c86
Tyler Ouellette <ouell117@uwindsor.ca>
2020-02-04 19:52:03 -0500

    exit on client side working

--- Python/client.py
+++ Python/client.py
@@ -1,26 +1,46 @@
 import socket
 import threading
 import sys
+import json
+
 
 class Client:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def sendMsg(self):
         while True:
-            self.sock.send(bytes(input(""), 'utf-8'))
-    
-    def __init__(self, address):
-        self.sock.connect((address, 8001))
+            userCommand = input("Enter command, type exit to exit: ")
+            self.sock.send(bytes(userCommand, 'utf-8'))
 
-        #iThread= input thread
+            if userCommand == 'exit':
+                print('Attemping to close')
+                self.sock.shutdown(1)
+                self.sock.close()
+                break
+
+            # length = len(userInput)
+            # lengthHeader = bytearray('\x00\x00\x00')
+            # lengthHeader.append(bytes(length))
+
+            # finalMessage = bytearray()
+            # finalMessage.append(lengthHeader)
+            # finalMessage.append(bytes(userInput, 'utf-8'))
+            # message = userInput.len(s.encode('utf-8'))
+        sys.exit(0)
+
+    def __init__(self, address):
+        self.sock.connect((address, 8002))
+
+        # iThread= input thread
         iThread = threading.Thread(target=self.sendMsg)
-        iThread.daemon=True
+        iThread.daemon = True
         iThread.start()
 
         while True:
-            data=self.sock.recv(1024)
+            data = self.sock.recv(1024)
             if not data:
                 break
-            print(str(data,'utf-8'))
+            print(str(data, 'utf-8'))
+
 
 client = Client(sys.argv[1])
--- Python/node.py
+++ Python/node.py
@@ -1,37 +1,38 @@
-#node.py
+# node.py
 
 import socket
 import threading
 from KeyVal import *
 import sys
 
+
 class Server:
-    #AF_INET says we're using IPV4
-    #SOCK_STREAM says we're using TCP
+    # AF_INET says we're using IPV4
+    # SOCK_STREAM says we're using TCP
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connections=[]
+    connections = []
     keyval = KeyVal()
     lock = threading.Lock()
 
     def __init__(self):
-        #binding socket to a port
-        #setting address to 0.0.0.0 makes it available to any IP configured on the server
-        self.sock.bind(('0.0.0.0',8001))
+        # binding socket to a port
+        # setting address to 0.0.0.0 makes it available to any IP configured on the server
+        self.sock.bind(('0.0.0.0', 8002))
         self.sock.listen(1)
 
     def convert(self, s, c):
-        arr=s.split()
-        command=arr[0]+'('
-        for i in range(1,len(arr)):
+        arr = s.split()
+        command = arr[0]+'('
+        for i in range(1, len(arr)):
             # try:
             #     int(arr[i])
             #     command+=arr[i]
             # except:
             #     command+="'"+arr[i]+"'"
             command += "'"+arr[i]+"'"
-            if i<len(arr)-1:
-                command+=','
-        command+=')'
+            if i < len(arr)-1:
+                command += ','
+        command += ')'
         print('self.keyval.'+command)
         self.lock.acquire()
         try:
@@ -42,18 +43,19 @@ class Server:
             self.lock.release()
             return 'Error'
 
-        
-
-
-    def handler(self,c,a):
+    def handler(self, c, a):
         while True:
-            #data received and max ammount in bytes
+            # data received and max ammount in bytes
             try:
                 data = c.recv(1024)
                 text = str(data, 'utf-8')
-                if text == 'exit': sys.exit(0)
-                result = self.convert(text,c)
-                #print(str(data,'utf-8'))
+                if text == 'exit':
+                    self.connections.remove(c)
+                    c.close()
+                    print(str(a[0])+':'+str(a[1])+" Disconnected")
+                    return
+                result = self.convert(text, c)
+                # print(str(data,'utf-8'))
                 c.send(bytes(result.encode()))
                 if not data:
                     self.connections.remove(c)
@@ -69,9 +71,9 @@ class Server:
 
     def run(self):
         while True:
-            #c is the connection, a is client's address
+            # c is the connection, a is client's address
             c, a = self.sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(c,a))
+            cThread = threading.Thread(target=self.handler, args=(c, a))
             cThread.daemon = True
             cThread.start()
             self.connections.append(c)
